80af466d514428e3b5c4c1ed03a76d1430ebc3fa
Bilel Jegham <bilel.jegham@niji.fr>
2020-07-20 12:59:37 +0200

    MakeFile asset

--- templates/assets/gulpfile.js
+++ templates/assets/gulpfile.js
@@ -10,11 +10,15 @@ const plumber = require("gulp-plumber");
 const rename = require("gulp-rename");
 const sass = require("gulp-sass");
 const uglify = require("gulp-uglify");
+const sourcemaps = require('gulp-sourcemaps');
+
+// Load package.json for banner
+const pkg = require('./package.json');
 
 // Set the banner content
 const banner = ['/*!\n',
-  ' * IdéSYS  - <%= pkg.title %> v<%= pkg.version %> (<%= pkg.homepage %>)\n',
-  ' * Copyright 2020-' + (new Date()).getFullYear(), ' IdéSYS\n',
+  ' * IdéSYS  - <%= pkg.title %> v<%= pkg.version %>\n',
+  ' * Copyright 2020 -' + (new Date()).getFullYear(), ' IdéSYS\n',
   ' */\n',
   '\n'
 ].join('');
@@ -22,11 +26,13 @@ const banner = ['/*!\n',
 // BrowserSync
 function browserSync(done) {
   browsersync.init({
-    server: {
-      baseDir: "./"
-    },
-    port: 3000
+
+    proxy: {
+      target: "localhost:5000", // can be [virtual host, sub-directory, localhost with port]
+      ws: true // enables websockets
+    }
   });
+
   done();
 }
 
@@ -41,22 +47,28 @@ function css() {
   return gulp
     .src("./scss/**/*.scss")
     .pipe(plumber())
+    .pipe(sourcemaps.init())
     .pipe(sass({
       outputStyle: "expanded",
       includePaths: "./node_modules",
     }))
+    .pipe(sourcemaps.write('.'))
     .on("error", sass.logError)
     .pipe(autoprefixer({
       cascade: false
     }))
-    .pipe(header(banner))
-    .pipe(gulp.dest("./css"))
+    .pipe(header(banner, {
+      pkg: pkg
+    }))
+    .pipe(gulp.dest("../../static/css"))
     .pipe(rename({
       suffix: ".min"
     }))
     .pipe(cleanCSS())
     .pipe(gulp.dest("../../static/css"))
-    .pipe(browsersync.stream());
+    .pipe(browsersync.reload({
+      stream: true
+    }));
 }
 
 // JS task
@@ -67,18 +79,25 @@ function js() {
       '!./js/*.min.js',
     ])
     .pipe(uglify())
-    .pipe(header(banner))
+    .pipe(header(banner, {
+      pkg: pkg
+    }))
     .pipe(rename({
       suffix: '.min'
     }))
-    .pipe(gulp.dest('../../static'))
-    .pipe(browsersync.stream());
+    .pipe(gulp.dest('../../static/js'))
+    .pipe(browsersync.reload({
+      stream: true
+    }));
 }
 
 // Watch files
 function watchFiles() {
   gulp.watch("./scss/**/*", css);
   gulp.watch(["./js/**/*", "!./js/**/*.min.js"], js);
+
+  gulp.watch("../../**/*.py").on('change', browsersync.reload);
+  gulp.watch("../**/*.html").on('change', browsersync.reload);
 }
 
 // Define complex tasks
@@ -88,7 +107,6 @@ const watch = gulp.series(build, gulp.parallel(watchFiles, browserSync));
 // Export tasks
 exports.css = css;
 exports.js = js;
-exports.vendor = vendor;
 exports.build = build;
 exports.watch = watch;
 exports.default = build;
